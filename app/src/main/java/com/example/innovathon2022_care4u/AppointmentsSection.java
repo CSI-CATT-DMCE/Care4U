d4af7134cb5c4a1dc819248347349a191156a66a
Amit <amitkulkarni2028@gmail.com>
2022-10-29 21:10:58 +0530

    Add dynamic Jitsi room codes

--- app/src/main/java/com/example/innovathon2022_care4u/AppointmentsSection.java
+++ app/src/main/java/com/example/innovathon2022_care4u/AppointmentsSection.java
@@ -2,7 +2,12 @@ package com.example.innovathon2022_care4u;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.airbnb.lottie.model.animatable.AnimatablePointValue;
 
 public class AppointmentsSection extends AppCompatActivity {
 
@@ -11,5 +16,14 @@ public class AppointmentsSection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointments_section);
+
+        Button launch_jitsi_button = findViewById(R.id.launch_jitsi_button);
+        launch_jitsi_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(AppointmentsSection.this, JitsiMiddleRoom.class);
+                startActivity(i);
+            }
+        });
     }
 }
